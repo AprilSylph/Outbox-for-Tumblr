5a11efadd800fe5e4208bd991e8c75ecdd877561
April Sylph <april@sylph.tech>
2021-04-13 19:13:56 +0100

    save NPF asks via webRequest

---
+++ src/background.js
@@ -0,0 +1,23 @@
+browser.webRequest.onBeforeRequest.addListener(({ method, url, requestBody }) => {
+  if (method !== 'POST') { return; }
+
+  const { pathname } = new URL(url);
+  const recipient = pathname.split('/')[4];
+
+  const decoder = new TextDecoder();
+
+  const rawData = requestBody.raw[0].bytes;
+  const decodedData = decoder.decode(rawData);
+  const parsedData = JSON.parse(decodedData);
+  const { state, content } = parsedData;
+
+  if (state === 'ask' && Array.isArray(content)) {
+    const timestamp = `${(new Date()).getTime()}`;
+    browser.storage.local.set({ [timestamp]: { recipient, content } });
+  }
+}, {
+  urls: ['*://www.tumblr.com/api/v2/blog/*/posts'],
+  types: ['xmlhttprequest']
+}, [
+  'requestBody'
+]);
