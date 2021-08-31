1264aebb59c1e1ea9f65ef4cb2e1c2f49b9e4676
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2021-08-31 15:59:10 +0100

    manage page: populate item footers
    - adds timestamps
    - adds Delete buttons

--- src/manage.js
+++ src/manage.js
@@ -2,8 +2,29 @@ import { renderContent } from './lib/npf.js';
 
 const mainElement = document.querySelector('main');
 
+const timeFormat = new Intl.DateTimeFormat(undefined, {
+  day: 'numeric',
+  month: 'short',
+  year: 'numeric',
+  hour: 'numeric',
+  minute: '2-digit'
+});
+
+const onDeleteButtonClicked = ({ currentTarget }) => {
+  if (!window.confirm('Delete this copy of your sent message?\nThis cannot be undone!')) {
+    return;
+  }
+
+  const articleElement = currentTarget.closest('article');
+  const { timestamp } = articleElement.dataset;
+  articleElement.remove();
+
+  browser.storage.local.remove(timestamp);
+};
+
 const constructItem = ([timestamp, { recipient, content, layout }]) => {
   const articleElement = document.createElement('article');
+  Object.assign(articleElement.dataset, { timestamp });
 
   if (recipient) {
     const headerElement = document.createElement('header');
@@ -42,6 +63,14 @@ const constructItem = ([timestamp, { recipient, content, layout }]) => {
 
   const footerElement = document.createElement('footer');
   articleElement.appendChild(footerElement);
+
+  const deleteButton = Object.assign(document.createElement('button'), { textContent: 'Delete' });
+  deleteButton.addEventListener('click', onDeleteButtonClicked);
+
+  const timestampDate = new Date(parseInt(timestamp));
+  const timestampElement = Object.assign(document.createElement('span'), { textContent: timeFormat.format(timestampDate) });
+
+  footerElement.append(timestampElement, deleteButton);
 
   return articleElement;
 };
