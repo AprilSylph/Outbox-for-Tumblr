8833d64fa33de40497a630276e8a01219d987588
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2021-10-08 16:39:59 +0100

    load navigation js as module
    defers the script and eliminates need for explicit isolation block

--- src/lib/navigation.js
+++ src/lib/navigation.js
@@ -1,29 +1,27 @@
-{
-  const articles = document.getElementsByTagName('article');
+const articles = document.getElementsByTagName('article');
 
-  document.documentElement.addEventListener('keydown', ({ currentTarget, key, target }) => {
-    if (target.value !== undefined) return;
+document.documentElement.addEventListener('keydown', ({ currentTarget, key, target }) => {
+  if (target.value !== undefined) return;
 
-    const scrollPaddingTop = parseInt(getComputedStyle(currentTarget).getPropertyValue('scroll-padding-top'));
+  const scrollPaddingTop = parseInt(getComputedStyle(currentTarget).getPropertyValue('scroll-padding-top'));
 
-    switch (key) {
-      case 'j': {
-        const targetNode = [...articles].find(childNode => Math.floor(childNode.getBoundingClientRect().y) > scrollPaddingTop);
-        targetNode?.scrollIntoView();
-        targetNode?.focus();
-        break;
-      }
-      case 'k': {
-        const targetNode = [...articles].reverse().find(childNode => Math.floor(childNode.getBoundingClientRect().y) < scrollPaddingTop);
-        targetNode?.scrollIntoView();
-        targetNode?.focus();
-        break;
-      }
-      case '.': {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-        document.activeElement?.blur();
-        break;
-      }
+  switch (key) {
+    case 'j': {
+      const targetNode = [...articles].find(childNode => Math.floor(childNode.getBoundingClientRect().y) > scrollPaddingTop);
+      targetNode?.scrollIntoView();
+      targetNode?.focus();
+      break;
     }
-  });
-}
+    case 'k': {
+      const targetNode = [...articles].reverse().find(childNode => Math.floor(childNode.getBoundingClientRect().y) < scrollPaddingTop);
+      targetNode?.scrollIntoView();
+      targetNode?.focus();
+      break;
+    }
+    case '.': {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+      document.activeElement?.blur();
+      break;
+    }
+  }
+});
